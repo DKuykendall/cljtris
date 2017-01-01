96946fcfa8544cb44a7ac667de0b8f83eccba047
Derek Kuykendall <admiralfisticuffs@gmail.com>
2017-01-01 01:01:08 -0500

    falling

--- src/cljtris/core.cljs
+++ src/cljtris/core.cljs
@@ -2,29 +2,58 @@
 
 (def size 50)
 
-(def line {:color "red" :squares [[0 0] [1 0] [2 0] [3 0]]})
+(def board (r/atom []))
 
-(def square {:color "blue" :squares [[0 0] [1 0] [0 1] [1 1]]})
+(defn create-piece [color squares] {:active true
+                                    :squares(for [[x y] squares]
+                                              {:x x
+                                               :y y
+                                               :color color
+                                               :active true})})
 
-(def s {:color "green" :squares [[0 1] [1 1] [1 0] [2 0]]})
+(def line (create-piece "red" [[0 0] [1 0] [2 0] [3 0]]))
 
-(def z {:color "gray" :squares [[0 0] [1 0] [1 1] [2 1]]})
+(def square (create-piece "blue" [[0 0] [1 0] [0 1] [1 1]]))
 
-(def L {:color "orange" :squares [[0 0] [0 1] [0 2] [1 2]]})
+(def s (create-piece "green" [[0 1] [1 1] [1 0] [2 0]]))
 
-(def reverse-L {:color "purple" :squares [[1 0] [1 1] [1 2] [0 2]]})
+(def z (create-piece "gray" [[0 0] [1 0] [1 1] [2 1]]))
+
+(def L (create-piece "orange" [[0 0] [0 1] [0 2] [1 2]]))
+
+(def reverse-L (create-piece "purple" [[1 0] [1 1] [1 2] [0 2]]))
 
 (def pieces [line square s z L reverse-L])
 
-(defn render [piece] 
-  (for [[x y] (:squares piece)] 
-    [:div {:style {:width (str (- size 2) "px")
-                   :height (str (- size 2) "px")
-                   :background-color (:color piece)
-                   :position "absolute"
-                   :left (inc (* size (inc x)))
-                   :top  (inc (* size (inc y)))}}]))
+(defn new-piece [] (swap! board conj (rand-nth pieces)))
 
-(defn first-component [] (into [:div] (render (rand-nth pieces))))
+(defn render [board] 
+  (for [piece board]
+    (for [{x :x y :y color :color} (:squares piece)] 
+      [:div {:style {:width (str (- size 2) "px")
+                     :height (str (- size 2) "px")
+                     :background-color color
+                     :position "absolute"
+                     :left (inc (* size (inc x)))
+                     :top  (inc (* size (inc y)))}
+             :key (gensym)}])))
+
+(defn fall [board] (for [piece board] 
+                     (if (:active piece)
+                       (let [new-squares 
+                             (for [square (:squares piece)]
+                               (assoc square :y (inc (:y square))))]
+                         (if (some #(> (:y %) 10) new-squares)
+                           (do (new-piece) {:active false :squares (:squares piece)})
+                           {:active true :squares new-squares}))
+                       piece)))
+
+(defn swap [] (swap! board fall))
+
+(defonce fall-event (js/setInterval swap 1000))
+
+(new-piece)
+
+(defn first-component [] (into [:div] (render @board)))
 
 (r/render-component [first-component] (.-body js/document))
