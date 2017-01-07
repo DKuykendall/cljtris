c930402f7fb1b7328919549ef867f622aac6a8a3
Derek Kuykendall <admiralfisticuffs@gmail.com>
2017-01-07 18:20:47 -0500

    Refactored data structure, arrows move pieces

--- src/cljtris/core.cljs
+++ src/cljtris/core.cljs
@@ -6,13 +6,12 @@
 
 (def width 10)
 
-(def board (r/atom []))
+(def board (r/atom {:active nil :blocks []}))
 
-(defn create-piece [color squares] {:active true
-                                    :squares(for [[x y] squares]
+(defn create-piece [color squares] (for [[x y] squares]
                                               {:x x
                                                :y y
-                                               :color color })})
+                                               :color color }))
 
 (def line (create-piece "red" [[0 0] [1 0] [2 0] [3 0]]))
 
@@ -28,26 +27,42 @@
 
 (def pieces [line square s z L reverse-L])
 
-(defn new-piece [] (swap! board conj (rand-nth pieces)))
+(defn add-piece [board] (assoc board :active (rand-nth pieces)))
 
-(defn render [board] 
-  (for [piece board]
-    (for [{x :x y :y color :color} (:squares piece)] 
-      [:div {:style {:width (str (- size 2) "px")
-                     :height (str (- size 2) "px")
-                     :background-color color
-                     :position "absolute"
-                     :left (inc (* size (inc x)))
-                     :top  (inc (* size (inc y)))}
-             :key (gensym)}])))
+(defn create-block [{x :x y :y color :color}]
+        [:div {:style {:width (str (- size 2) "px")
+                       :height (str (- size 2) "px")
+                       :background-color color
+                       :position "absolute"
+                       :left (inc (* size (inc x)))
+                       :top  (inc (* size (inc y)))}
+               :key (gensym)}])
 
-(defn move [dir] (println dir))
+(defn render [board]
+  (let [active-squares (for [square (:active board)] (create-block square))
+        pile-squares   (for [square (:blocks board)] (create-block square))]
+    (concat active-squares pile-squares)))
+
+(defn on-other-piece? [squares blocks]
+  (let [result (for [square squares]
+                 (seq (filter #(and (= (:x square) (:x %))
+                                    (= (:y square) (:y %))) 
+                              blocks)))]
+    (seq (remove nil? result))))
+
+(defn move [dir] 
+  (swap! board assoc :active  (let [new-piece (for [square (:active @board)] 
+                             (assoc square :x (+ dir (:x square))))]
+             (if (or (some #(or (< (:x %) 0) (> (:x %) (dec width))) new-piece)
+                     (on-other-piece? new-piece (:blocks @board)))
+               (:active @board)
+               new-piece))))
 
 (defn rotate [])
 
 (defn fast-drop [])
 
-(defn keydown [e] 
+(defn keydown [e]
   (case (.-keyCode e)
     ;left
     37 (move -1)
@@ -59,32 +74,28 @@
     40 (fast-drop)
     nil))
 
-(defn on-other-piece? [squares board] 
-  (let [result (for [square squares
-                     piece board]
-                 (seq (filter #(and (= (:x square) (:x %))
-                               (= (:y square) (:y %))
-                               (not (:active piece))) (:squares piece))))]
-    (seq (remove nil? result))))
 
-(defn fall [board] (for [piece board] 
-                     (if (:active piece)
-                       (let [new-squares 
-                             (for [square (:squares piece)]
-                               (assoc square :y (inc (:y square))))]
-                         (if (or (some #(> (:y %) (dec height)) new-squares)
-                                 (on-other-piece? new-squares board))
-                           (do (new-piece) {:active false :squares (:squares piece)})
-                           {:active true :squares new-squares}))
-                       piece)))
+(defn fall [board] (let [piece (:active board)
+                         new-piece
+                         (for [square piece]
+                           (assoc square :y (inc (:y square))))]
+                     (if (or (some #(> (:y %) (dec height)) new-piece)
+                             (on-other-piece? new-piece (:blocks board)))
+                       (add-piece (assoc board :blocks (concat (:active board)
+                                                               (:blocks board))))
+                       (assoc board :active new-piece))))
+                     ;(if (or (some #(> (:y %) (dec height)) new-squares)
+                     ;        (on-other-piece? new-squares board))
+                     ;  (do (new-piece) {:active false :squares (:squares piece)})
+                     ;  {:active true :squares new-squares})))
 
 (defn swap [] (swap! board fall))
 
-(defonce fall-event (js/setInterval swap 1000))
+(defonce fall-event (js/setInterval swap 200))
 
 (defonce key-event (.addEventListener js/document "keydown" keydown))
 
-(new-piece)
+(swap! board add-piece @board)
 
 (defn first-component [] (into [:div] (render @board)))
 
